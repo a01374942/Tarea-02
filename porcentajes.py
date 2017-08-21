a22cce51787978234fe9343afe74300dfe81424d
Angel Pesado <A01374942@itesm.mx>
2017-08-20 22:04:24 -0500

    El código 4 sobre el numero de hombres y mujeres, está acabado.

--- porcentajes.py
+++ porcentajes.py
@@ -1,8 +1,15 @@
 #encoding: UTF-8
 
 # Autor: Angel Roberto Pesado Bartolo, A01374942
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Descripcion: Realizaré un programa que imprima el porcentaje de mujeres y hombres que hay en una clase, dado por el usuario.
 
 # A partir de aquí escribe tu programa
 
-print("hola :v ")
+hombres = int(input("Dame el numero de hombres dentro de la clase: "))
+mujeres = int(input("Dame el numero de mujeres dentro de la clase: "))
+total = (hombres + mujeres)
+porcentajeh = (100*hombres)/(total)
+porcentajem = (100*mujeres)/(total)
+print("El total de alumnos en clase es de: ",total,"alumnos")
+print("El porcentaje de hombres es de:",porcentajeh,"%")
+print("El porcentaje de mujeres es de:",porcentajem,"%")
