0ddb258b4dec93a04573626050599d7b79d801da
Angel Pesado <A01374942@itesm.mx>
2017-08-20 22:02:22 -0500

    El código 3 de la cuenta, propina, iva, está acabado.

--- cuenta.py
+++ cuenta.py
@@ -1,10 +1,15 @@
 #encoding: UTF-8
 
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Angel Roberto Pesado Bartolo, A01374942
+# Descripcion: Primero debemos pedir al usuario el gastó para después obtener la iva y la propina, para finalmente imprimir el gasto total..
 
 # A partir de aquí escribe tu programa
 
-a= float(input("dame un numero"))
-b= (a)+(km)
-print("h",b)
+subtotal=float(input("Dime cuanto gastaste en el restarurante: "))
+propina=(subtotal*.12)
+iva= (subtotal*.16)
+total= (subtotal+propina+iva)
+print("Tu subtotal es de: ",subtotal,"pesos")
+print("Tu propina a pagar es:",propina,"pesos")
+print("Tu IVA a pagar es de: ",iva,"pesos")
+print("Tu TOTAL a pagar es de ",total,"pesos")
