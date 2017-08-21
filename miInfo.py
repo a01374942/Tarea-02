003f1cfe4e2ef85dd3a2a3f66f5758ff415e64ff
Angel Pesado <A01374942@itesm.mx>
2017-08-20 20:04:17 -0500

    El código 1 está terminado

--- miInfo.py
+++ miInfo.py
@@ -1,7 +1,19 @@
 #encoding: UTF-8
 
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+
+# Autor:Angel Roberto Pesado Bartolo, A01374942
+# Descripcion: Imprimir mi información personal a base de un código de salidas.
 
 # A partir de aquí escribe tu programa
 
+a = str ("Angel Roberto Pesado Bartolo")
+b = str ("A01374942")
+c = str ("ISDR")
+d = str ("Prepa Tec, Programa Bicultural")
+e = str("Me gusta el futbol, le voy a las Chivas, tambien me gusta la electronica y me gusta ver series.")
+
+print("Mi nombre es:",a)
+print("Matricula:",b)
+print("Carrera:",c)
+print("Estudie en:",d)
+print("Descripcion:",e)
